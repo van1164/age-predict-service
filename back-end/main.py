a6d4b10ff539e7c8e9f337b16f55eb5393482d82
van1164 <vkn123@naver.com>
2023-12-15 01:49:05 +0900

    [feat] 모델 불러와서 예측 구현

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from io import BytesIO
+import torchvision.transforms as transforms
+import torch
+import timm
+origins = ["*"]
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+@app.post("/upload")
+async def upload_photo(file: UploadFile):
+    image= await file.read()
+    image = Image.open(BytesIO(image))
+    
+    # 이미지를 텐서로 변환합니다.
+    transform = transforms.Compose([
+        transforms.Resize((256)),
+        transforms.CenterCrop((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    tensor_image = transform(image)
+    print(tensor_image.shape)
+    model = timm.create_model("efficientvit_b0",pretrained=False, num_classes=70)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    loaded_model = torch.load('face_model.pth', map_location=torch.device('cpu'))
+    model.load_state_dict(loaded_model)
+    #loaded_model.eval()
+    print(model)
+
+    output = model(tensor_image.unsqueeze(0))
+    print(output.argmax(-1))
+    return {"age":int(output.argmax(-1)) }
