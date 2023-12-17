c36a932e95d2d7d8a7740809ad39bd46ce495ef1
van1164 <vkn123@naver.com>
2023-12-17 19:41:41 +0900

    [Feat] 모델 크기 증가

--- back-end/main.py
+++ back-end/main.py
@@ -5,6 +5,10 @@ from io import BytesIO
 import torchvision.transforms as transforms
 import torch
 import timm
+import torch.nn as nn
+import torchvision
+from sampleCNN import SampleCNN
+from EnsembleModel import EnsembleModel
 origins = ["*"]
 
 
@@ -17,24 +21,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+
+model1 = SampleCNN()
+model2=torchvision.models.resnet18(torchvision.models.ResNet18_Weights)
+model2.fc = nn.Sequential(
+    nn.Linear(512,512),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.Linear(512,70),
+    nn.Softmax()
+)
+
+model = EnsembleModel(model1,model2,70)
+
 @app.post("/upload")
 async def upload_photo(file: UploadFile):
     image= await file.read()
     image = Image.open(BytesIO(image))
     
-    # 이미지를 텐서로 변환합니다.
+    # transform = transforms.Compose([
+    #     transforms.Resize((256)),
+    #     transforms.CenterCrop((224,224)),
+    #     transforms.ToTensor(),
+    #     #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
+
     transform = transforms.Compose([
-        transforms.Resize((256)),
-        transforms.CenterCrop((224,224)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+   transforms.Resize((128, 128)),
+   #transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.2),
+   transforms.ToTensor(),
+])
+
     tensor_image = transform(image)
     print(tensor_image.shape)
-    model = timm.create_model("efficientvit_b0",pretrained=False, num_classes=70)
+    #model = timm.create_model("efficientvit_b0",pretrained=False, num_classes=70)
 
-    loaded_model = torch.load('face_model.pth', map_location=torch.device('cpu'))
+    #loaded_model = torch.load('face_model.pth', map_location=torch.device('cpu'))
+    #loaded_model = torch.load('resnet18.pth', map_location=torch.device('cpu'))
+    loaded_model = torch.load('ensamble.pth', map_location=torch.device('cpu'))
     model.load_state_dict(loaded_model)
+    model.eval()
     #loaded_model.eval()
     print(model)
 
